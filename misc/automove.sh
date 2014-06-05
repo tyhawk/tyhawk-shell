edc76c60cc4e98212a8b3efcd6b5b36f172ff041
John Gerritse <tyrannis.hawk@gmail.com>
2014-06-05 12:07:32 +0200

    Prettyfied output and added extra checks. Now keeps required Plex TV-Show directory structure in mind and creates dirs where required.

--- misc/automove.sh
+++ misc/automove.sh
@@ -26,6 +26,27 @@ STAGING="$HOME/Staging"
 tvshowroot="/media/EHD01/Plex/TV-Shows"
 movieroot="/media/EHD02/Plex/Movies"
 
+# Dependency check
+hash rsync 2>/dev/null || { printf "Could not find rsync.\nPlease make sure it is installed.\nAborting ..." >&2; exit 1; }
+
+# Check if root dirs exist
+if [[ ! -d "$tvshowroot" ]]; then
+    printf "Directory $tvshowroot not found: "; do_error
+    exit 1
+fi
+if [[ ! -d "$movieroot" ]]; then
+    printf "Directory $movieroot not found: "; do_error
+    exit 1
+fi
+
+   ##    #    #   #####   ####   #    #   ####   #    #  ######
+  #  #   #    #     #    #    #  ##  ##  #    #  #    #  #
+ #    #  #    #     #    #    #  # ## #  #    #  #    #  #####
+ ######  #    #     #    #    #  #    #  #    #  #    #  #
+ #    #  #    #     #    #    #  #    #  #    #   #  #   #
+ #    #   ####      #     ####   #    #   ####     ##    ######
+
+
 printf "Starting automove.\n"
 
 # First check if there are MKV files in the Staging dir
@@ -39,15 +60,21 @@ tvshows=( $(basename "$(ls $STAGING/*-s..e..-*mkv)" ) )
 movies=( $(basename "$(ls $STAGING/*-\(....\).mkv)" ) )
 
 # We copy the TV-Show to its proper directory one by one and automatically.
-for tvshow in "${tvshows[@]}"
+for tvshowfile in "${tvshows[@]}"
 do
     # Extract the show's proper name from the variable
-    showname=$(echo "$tvshow" | cut --delimiter=\- --fields=1)
-    # We locate the proper dir for the tv show
-    showlocation=$(find /$tvshowroot/* -name $showname -type d)
+    showname=$(echo "$tvshowfile" | cut --delimiter=\- --fields=1)
+    # Extract season as well
+    showepnum=$(echo "$tvshowfile" | cut --delimiter=\- --fields=2)
+    season="$((10#${showepnum:1:2}))"
+    # Check if the location exists and if not, create it
+    showlocation="$tvshowroot/$showname/Season$season"
+    if [[ ! -d "$showlocation" ]]; then
+        mkdir -p $showlocation
+    fi
     # We move the file to its proper location
     printf "Moving file $tvshow to $showlocation:\n"
-    rsync --perms --times --progress --human-readable --compress --remove-source-files --partial $STAGING/$tvshow $showlocation
+    rsync --perms --times --progress --human-readable --compress --remove-source-files --partial $STAGING/$tvshowfile $showlocation
     if [[ "$?" -gt 0 ]]; then
         printf "  Transfer completed: "; do_error
     else
@@ -56,11 +83,11 @@ do
 done
 
 # Movies are easier, they all go into one directory
-for movie in "${movies[@]}"
+for moviefile in "${movies[@]}"
 do
         # We move the file to its proper location
-    printf "Moving file $movie to $movieroot:\n"
-    rsync --perms --times --progress --human-readable --compress --remove-source-files --partial $STAGING/$movie $movieroot
+    printf "Moving file $moviefile to $movieroot:\n"
+    rsync --perms --times --progress --human-readable --compress --remove-source-files --partial $STAGING/$moviefile $movieroot
     if [[ "$?" -gt 0 ]]; then
         printf "  Transfer completed: "; do_error
     else
