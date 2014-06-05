1872d23269a7d06037a3f819c9fbf30632e5888d
John Gerritse <tyrannis.hawk@gmail.com>
2014-06-05 18:54:33 +0200

    Modified indexing file code.

--- misc/automove.sh
+++ misc/automove.sh
@@ -81,8 +81,8 @@ if [[ $(find $STAGING/*mkv | wc -l) -eq 0 ]]; then
 fi
 
 # Now we split them up into movies and tv shows
-tvshows=( $(basename "$(ls $STAGING/*-s??e??-*mkv)" ) )
-movies=( $(basename "$(ls $STAGING/*-\(????\).mkv)" ) )
+tvshows=( $(basename "$(find $STAGING/ -type f -name '*-s??e??-*.mkv' -exec basename {} \;)" ) ) # Add code to catch no matching files
+movies=( $(basename "$(find $STAGING/ -type f -name '*-\(....\).mkv' -exec basename {} \;)" ) ) # Add code to catch no matching files
 
 # We copy the TV-Show to its proper directory one by one and automatically.
 for tvshowfile in "${tvshows[@]}"
