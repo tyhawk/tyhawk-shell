9c675c8c66408f5fe8099e7fa6a31d3b77158484
John Gerritse <tyrannis.hawk@gmail.com>
2014-10-27 20:32:21 +0100

    Automove can now be daemonized.

--- misc/automove.sh
+++ misc/automove.sh
@@ -2,7 +2,7 @@
 #
 # automove - Automatic media file distributor based on filename
 ############################################################################
-# © 2014 - John Gerritse
+# © 2014 - John Gerritse - version 1.2
 ############################################################################
 
 # Colours... oooh pretty!
@@ -19,29 +19,30 @@ bright=$(tput bold)
 exit_int() {
   # Function to do a clean exit when an interrupt is trapped
   printf "${red}Process aborted by user${normal}\n"
+  cleanup
   exit 0
 }
 
 exit_term() {
   # Function to do a clean exit when a termination is trapped
   echo "${red}Process terminated${normal}"
+  cleanup
   exit 1
 }
 
-do_error() {
-  printf "[${red}ERR${normal}]\n"
+cleanup () {
+  rm $lf
 }
 
-do_ok() {
-  printf "[${green}OK${normal}]\n"
-}
-
-# Define the staging directory
-staging="$HOME/Staging"
+#Staging directory
+staging="$HOME/PLEX/03_Finished/"
 
 # Define root dirs for TV shows & movies
-tvshowroot="/media/EHD01/Plex/TV-Shows"
-movieroot="/media/EHD02/Plex/Movies"
+tvshowroot="/mnt/TV"
+movieroot="/mnt/Movies"
+
+# Logfile
+logfile="$HOME/automove.log"
 
 ####
 # The script itself
@@ -50,16 +51,6 @@ movieroot="/media/EHD02/Plex/Movies"
 # Trap TERM, HUP, and INT signals and properly exit
 trap exit_term TERM HUP
 trap exit_int INT
-
-# Clear screen en print 'logo'
-clear
-printf "${blue}${bright}   ##    #    #   #####   ####   #    #   ####   #    #  ######\n"
-printf "  #  #   #    #     #    #    #  ##  ##  #    #  #    #  #\n"
-printf " #    #  #    #     #    #    #  # ## #  #    #  #    #  #####\n"
-printf " ######  #    #     #    #    #  #    #  #    #  #    #  #\n"
-printf " #    #  #    #     #    #    #  #    #  #    #   #  #   #\n"
-printf " #    #   ####      #     ####   #    #   ####     ##    ######\n"
-printf "\n     Move movie and tv-show files to the proper directory${normal}\n\n"
 
 # Dependency check - more code then before, but much nicer
 my_needed_commands="rsync"
@@ -79,61 +70,82 @@ fi
 
 # Check if root dirs exist
 if [[ ! -d "$tvshowroot" ]]; then
-  printf "Directory $tvshowroot not found: "; do_error
+  printf "Directory $tvshowroot not found: [ERROR]"
   exit 1
 fi
 if [[ ! -d "$movieroot" ]]; then
-  printf "Directory $movieroot not found: "; do_error
+  printf "Directory $movieroot not found: [ERROR]"
   exit 1
 fi
 
-# First check if there are MKV files in the Staging dir
-if [[ $(find $staging/*mkv | wc -l) -eq 0 ]]; then
-  printf "No MKV files found. Exiting.\n"
-  exit 0
-fi
-
-# Now we split them up into movies and tv shows
-tvshows=( $(find $staging/ -type f -name '*-s??e??-*.mkv' -exec basename {} \;) ) # Add code to catch no matching files
-movies=( $(find $staging/ -type f -name '*-\(????\).mkv' -exec basename {} \;) ) # Add code to catch no matching files
-
-# We copy the TV-Show to its proper directory one by one and automatically.
-for tvshowfile in "${tvshows[@]}"
+# Time to start our loop
+while true
 do
-  # Extract the show's proper name from the variable
-  showname=$(echo "$tvshowfile" | cut --delimiter=\- --fields=1)
-  # Extract season as well
-  showepnum=$(echo "$tvshowfile" | cut --delimiter=\- --fields=2)
-  season="${showepnum:1:2}"
-  # Check if the location exists and if not, create it
-  showlocation="$tvshowroot/$showname/Season$season"
-  if [[ ! -d "$showlocation" ]]; then
-    mkdir -p $showlocation
+  lf=/tmp/pidLockFile
+  # create empty lock file if none exists
+  if [[ ! -e $lf ]]; then
+    cat /dev/null >> $lf
   fi
-  # We move the file to its proper location
-  printf "Moving file $tvshow to $showlocation:\n"
-  rsync --perms --times --progress --human-readable --compress --remove-source-files --partial $staging/$tvshowfile $showlocation
-  if [[ "$?" -gt 0 ]]; then
-    printf "  Transfer completed: "; do_error
+  read lastPID < $lf
+  # if lastPID is not null and a process with that pid exists , exit
+  if [[ ! -z "$lastPID" ]] && [[ -d "/proc/$lastPID" ]]; then
+    printf "$(date +"%b %d %H:%M:%S") Automove already running. Exiting this instance.\n" >> $logfile
+    exit 0
   else
-    printf "  Transfer completed: "; do_ok
+    # save my pid in the lock file
+    echo $$ > $lf
+    # sleep just to make testing easier
+    sleep 5
+    # Time to start checking for files
+    # First check if there are MKV files in the Staging dir
+    if [[ $(find $staging/*mkv | wc -l) -eq 0 ]]; then
+      printf "$(date +"%b %d %H:%M:%S") No MKV files found. Sleeping for 60 seconds.\n" >> $logfile
+      sleep 60
+    else
+      # Now we split them up into movies and tv shows
+      tvshows=( $(find $staging/ -type f -name '*-s??e??-*.mkv' -exec basename {} \;) ) # Add code to catch no matching files
+      movies=( $(find $staging/ -type f -name '*-\(????\).mkv' -exec basename {} \;) ) # Add code to catch no matching files
+      # We copy the TV-Show to its proper directory one by one and automatically.
+      for tvshowfile in "${tvshows[@]}"
+      do
+        # Extract the show's proper name from the variable
+        showname=$(echo "$tvshowfile" | cut --delimiter=\- --fields=1)
+        # Extract season as well
+        showepnum=$(echo "$tvshowfile" | cut --delimiter=\- --fields=2)
+        season="${showepnum:1:2}"
+        # Check if the location exists and if not, create it
+        showlocation="$tvshowroot/$showname/Season$season"
+        if [[ ! -d "$showlocation" ]]; then
+          mkdir -p $showlocation
+        fi
+        # We move the file to its proper location
+        printf "$(date +"%b %d %H:%M:%S") moving file $tvshow to $showlocation\n" >> $logfile
+        rsync --perms --times --progress --human-readable --compress --remove-source-files --partial $staging/$tvshowfile $showlocation
+        if [[ "$?" -gt 0 ]]; then
+          printf "$(date +"%b %d %H:%M:%S") transfer of $tvshow failed\n" >> $logfile
+        else
+          printf "$(date +"%b %d %H:%M:%S") transfer of $tvshow completed\n" >> $logfile
+        fi
+      done
+      # Movies are easier, they all go into one directory
+      for moviefile in "${movies[@]}"
+      do
+        # We move the file to its proper location
+        printf "$(date +"%b %d %H:%M:%S") moving file $moviefile to $movieroot\n"  >> $logfile
+        rsync --perms --times --progress --human-readable --compress --remove-source-files --partial $staging/$moviefile $movieroot
+        if [[ "$?" -gt 0 ]]; then
+          printf "$(date +"%b %d %H:%M:%S") transfer of $moviefile failed\n" >> $logfile
+        else
+          printf "$(date +"%b %d %H:%M:%S") transfer of $moviefile completed\n" >> $logfile
+        fi
+      done
+      printf "$(date +"%b %d %H:%M:%S") moving file $moviefile to $movieroot\n"  >> $logfile
+   fi
+    # Now, we sleep for two minutes
+    sleep 120
   fi
-  printf "\n${blue}#${yellow}--------------------------------------------------------------${blue}#${normal}\n\n"
 done
 
-# Movies are easier, they all go into one directory
-for moviefile in "${movies[@]}"
-do
-    # We move the file to its proper location
-  printf "Moving file $moviefile to $movieroot:\n"
-  rsync --perms --times --progress --human-readable --compress --remove-source-files --partial $staging/$moviefile $movieroot
-  if [[ "$?" -gt 0 ]]; then
-    printf "  Transfer completed: "; do_error
-  else
-    printf "  Transfer completed: "; do_ok
-  fi
-  printf "\n${blue}#${yellow}--------------------------------------------------------------${blue}#${normal}\n\n"
-done
+cleanup
 
-printf "${blue}${bright}ALL FILES MOVED. EXITING.${normal}\n"
 ## END SCRIPT
