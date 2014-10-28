1e3988e5b22963581ec9bcda19ece61ac8681785
John Gerritse <tyrannis.hawk@gmail.com>
2014-10-28 09:51:59 +0100

    While loop should now work as intended.

--- misc/automove.sh
+++ misc/automove.sh
@@ -34,6 +34,46 @@ cleanup () {
   rm $lf
 }
 
+move_movie () {
+    # Movies are easier, they all go into one directory
+    for moviefile in "${movies[@]}"
+    do
+        # We move the file to its proper location
+        printf "$(date +"%b %d %H:%M:%S") moving file $moviefile to $movieroot\n"  >> $logfile
+        rsync --perms --times --progress --human-readable --compress --remove-source-files --partial $staging/$moviefile $movieroot
+        if [[ "$?" -gt 0 ]]; then
+            printf "$(date +"%b %d %H:%M:%S") transfer of $moviefile failed\n" >> $logfile
+        else
+            printf "$(date +"%b %d %H:%M:%S") transfer of $moviefile completed\n" >> $logfile
+        fi
+    done
+}
+
+move_tvshow () {
+    # We copy the TV-Show to its proper directory one by one and automatically.
+    for tvshowfile in "${tvshows[@]}"
+    do
+        # Extract the show's proper name from the variable
+        showname=$(echo "$tvshowfile" | cut --delimiter=\- --fields=1)
+        # Extract season as well
+        showepnum=$(echo "$tvshowfile" | cut --delimiter=\- --fields=2)
+        season="${showepnum:1:2}"
+        # Check if the location exists and if not, create it
+        showlocation="$tvshowroot/$showname/Season$season"
+        if [[ ! -d "$showlocation" ]]; then
+            mkdir -p $showlocation
+        fi
+        # We move the file to its proper location
+        printf "$(date +"%b %d %H:%M:%S") moving file $tvshow to $showlocation\n" >> $logfile
+        rsync --perms --times --progress --human-readable --compress --remove-source-files --partial $staging/$tvshowfile $showlocation
+        if [[ "$?" -gt 0 ]]; then
+            printf "$(date +"%b %d %H:%M:%S") transfer of $tvshow failed\n" >> $logfile
+        else
+            printf "$(date +"%b %d %H:%M:%S") transfer of $tvshow completed\n" >> $logfile
+        fi
+    done
+}
+
 #Staging directory
 staging="$HOME/PLEX/03_Finished/"
 
@@ -78,74 +118,48 @@ if [[ ! -d "$movieroot" ]]; then
   exit 1
 fi
 
-# Time to start our loop
-while true
-do
-  lf=/tmp/pidLockFile
-  # create empty lock file if none exists
-  if [[ ! -e $lf ]]; then
+# Check the lockfile
+lf=/tmp/pidLockFile
+# create empty lock file if none exists
+if [[ ! -e $lf ]]; then
     cat /dev/null >> $lf
-  fi
-  read lastPID < $lf
-  # if lastPID is not null and a process with that pid exists , exit
-  if [[ ! -z "$lastPID" ]] && [[ -d "/proc/$lastPID" ]]; then
-    printf "$(date +"%b %d %H:%M:%S") Automove already running. Exiting this instance.\n" >> $logfile
+fi
+# It exists, so we read the PID in the file into lastPID
+read lastPID < $lf
+
+# if lastPID is not null and a process with that pid exists , exit
+if [[ ! -z "$lastPID" ]] && [[ -d "/proc/$lastPID" ]]; then
+    printf "$(date +"%b %d %H:%M:%S") Automove already running ($lastPID).\n" >> $logfile
+    printf "$(date +"%b %d %H:%M:%S") Exiting this instance ($$).\n" >> $logfile
     exit 0
-  else
+else
     # save my pid in the lock file
     echo $$ > $lf
     # sleep just to make testing easier
     sleep 5
-    # Time to start checking for files
-    # First check if there are MKV files in the Staging dir
-    if [[ $(find $staging/*mkv | wc -l) -eq 0 ]]; then
-      printf "$(date +"%b %d %H:%M:%S") No MKV files found. Sleeping for 60 seconds.\n" >> $logfile
-      sleep 60
-    else
-      # Now we split them up into movies and tv shows
-      tvshows=( $(find $staging/ -type f -name '*-s??e??-*.mkv' -exec basename {} \;) ) # Add code to catch no matching files
-      movies=( $(find $staging/ -type f -name '*-\(????\).mkv' -exec basename {} \;) ) # Add code to catch no matching files
-      # We copy the TV-Show to its proper directory one by one and automatically.
-      for tvshowfile in "${tvshows[@]}"
-      do
-        # Extract the show's proper name from the variable
-        showname=$(echo "$tvshowfile" | cut --delimiter=\- --fields=1)
-        # Extract season as well
-        showepnum=$(echo "$tvshowfile" | cut --delimiter=\- --fields=2)
-        season="${showepnum:1:2}"
-        # Check if the location exists and if not, create it
-        showlocation="$tvshowroot/$showname/Season$season"
-        if [[ ! -d "$showlocation" ]]; then
-          mkdir -p $showlocation
-        fi
-        # We move the file to its proper location
-        printf "$(date +"%b %d %H:%M:%S") moving file $tvshow to $showlocation\n" >> $logfile
-        rsync --perms --times --progress --human-readable --compress --remove-source-files --partial $staging/$tvshowfile $showlocation
-        if [[ "$?" -gt 0 ]]; then
-          printf "$(date +"%b %d %H:%M:%S") transfer of $tvshow failed\n" >> $logfile
+    # Time to start our loop
+    while true
+    do
+        # Time to start checking for files
+        # First check if there are MKV files in the Staging dir
+        if [[ $(find $staging/*mkv | wc -l) -eq 0 ]]; then
+            printf "$(date +"%b %d %H:%M:%S") No MKV files found. Sleeping for 60 seconds.\n" >> $logfile
+            sleep 60
         else
-          printf "$(date +"%b %d %H:%M:%S") transfer of $tvshow completed\n" >> $logfile
+            # Move the movie files first
+            movies=( $(find $staging/ -type f -name '*-\(????\).mkv' -exec basename {} \;) )
+            if [[ -n "$movies" ]]; then
+                move_movie
+            fi
+            # Next, tv shows
+            tvshows=( $(find $staging/ -type f -name '*-s??e??-*.mkv' -exec basename {} \;) )
+            if [[ -n "$tvshow" ]]; then
+                move_tvshow
+            fi
+            # Now, we sleep for one minute
+            sleep 60
         fi
-      done
-      # Movies are easier, they all go into one directory
-      for moviefile in "${movies[@]}"
-      do
-        # We move the file to its proper location
-        printf "$(date +"%b %d %H:%M:%S") moving file $moviefile to $movieroot\n"  >> $logfile
-        rsync --perms --times --progress --human-readable --compress --remove-source-files --partial $staging/$moviefile $movieroot
-        if [[ "$?" -gt 0 ]]; then
-          printf "$(date +"%b %d %H:%M:%S") transfer of $moviefile failed\n" >> $logfile
-        else
-          printf "$(date +"%b %d %H:%M:%S") transfer of $moviefile completed\n" >> $logfile
-        fi
-      done
-      printf "$(date +"%b %d %H:%M:%S") moving file $moviefile to $movieroot\n"  >> $logfile
-   fi
-    # Now, we sleep for two minutes
-    sleep 120
-  fi
-done
-
+    done
+fi
 cleanup
-
 ## END SCRIPT
