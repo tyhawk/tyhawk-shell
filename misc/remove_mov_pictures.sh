34adf4783e02db0bdb7d8f70993072f24abceec5
John Gerritse <tyrannis.hawk@gmail.com>
2015-08-21 21:29:57 +0200

    Working version without testing code.

--- misc/remove_mov_pictures.sh
+++ misc/remove_mov_pictures.sh
@@ -2,7 +2,6 @@
 directory="/home/tyhawk/TransFusion/Spanje_2015"
 
 videobestanden=( $( find "$directory" -name '*MOV' -exec basename {} .MOV \; ) )
-echo "Array length: ${#videobestanden[@]}"
 
 for bestand in ${videobestanden[@]}
 do
