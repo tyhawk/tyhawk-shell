1dcc14456e26f11c39ac7a057c6145a66ad26a6a
John Gerritse <tyrannis.hawk@gmail.com>
2015-12-27 13:15:35 +0100

    Replaced tabs with 2 spaces.

--- misc/run_mediaprep.sh
+++ misc/run_mediaprep.sh
@@ -4,30 +4,30 @@ prequeue="/home/tyhawk/TransFusion/90_Prequeue"
 
 while :
 do
-	# First check to see if there is a file named "stop" in the workdir.
-	# This wil help us stop the script dead in its tracks
-	if [[ -e "$workdir/stop" ]]
-	then
-		exit 0
-	fi
-	# Check if there are any new files in the workdir
-	if [[ $(ls $workdir/ | wc -l) -eq 0 ]]
-	then
-		# Check if there are files in the pre-queue
-		if [[ $(ls $prequeue/ | wc -l) -eq 0 ]]
-		then
-			exit 0
-		else
-			# There are still files. Move the top file to the workdir
-			oldestfile=$(ls -rt "$prequeue" | head -n 1 | awk -F "-" '{ print $1 }')
-			filetomove=$(ls $prequeue/${oldestfile}* | sort -u | head -n 1)
-			basefile=$(basename $filetomove)
-			# Remove suffix so we don't miss any subtitles
-			filename="${basefile%.*}"
-			# Move the file (and subs if there are any) to the workdir
-			mv $prequeue/${filename}.* $workdir/
-		fi
-	fi
-        # Start MediaPrep
-        $HOME/bin/mediaprep
+  # First check to see if there is a file named "stop" in the workdir.
+  # This wil help us stop the script dead in its tracks
+  if [[ -e "$workdir/stop" ]]
+  then
+    exit 0
+  fi
+  # Check if there are any new files in the workdir
+  if [[ $(ls $workdir/ | wc -l) -eq 0 ]]
+  then
+    # Check if there are files in the pre-queue
+    if [[ $(ls $prequeue/ | wc -l) -eq 0 ]]
+    then
+      exit 0
+    else
+      # There are still files. Move the top file to the workdir
+      oldestfile=$(ls -rt "$prequeue" | head -n 1 | awk -F "-" '{ print $1 }')
+      filetomove=$(ls $prequeue/${oldestfile}* | sort -u | head -n 1)
+      basefile=$(basename $filetomove)
+      # Remove suffix so we don't miss any subtitles
+      filename="${basefile%.*}"
+      # Move the file (and subs if there are any) to the workdir
+      mv $prequeue/${filename}.* $workdir/
+    fi
+  fi
+  # Start MediaPrep
+  $HOME/bin/mediaprep
 done
