bc0cf773ba34794ab285086e93208c5301c7ee16
John Gerritse <tyrannis.hawk@gmail.com>
2016-01-10 19:53:37 +0100

    Decided on Mindf.ck.

--- misc/mediaprep.sh
+++ misc/mediaprep.sh
@@ -55,7 +55,7 @@ bright=$(tput bold)
 
 # TV shows that have no subs
 tvnosubs=( "Home.Videos" "Moordvrouw" "Smeris" "Toren.C" "Divorce" "de.Man.met.de.Hamer" "Celblok.H" "Komt.Een.Man.Bij.De.Dokter" "My.Cat.From.Hell" "Witchblade" "Tom.and.Jerry" "Looney.Tunes" "Nieuwe.Buren" "Het.Zandkasteel" "Bluf" "Gooische.Vrouwen" "S1ngle" "Dokter.Tinus" "Penoza" "Baantjer" "Vrienden.Voor.Het.Leven" "Wie.Is.De.Mol" "Swiebertje" "Missie.Aarde" "Danni.Lowinski.NL" "Familie.Kruys" "Zwarte.Tulp" "Voetbalvrouwen" "Draadstaal" "Zie.Ze.Vliegen" "the.Joy.of.Painting"
-"MythBusters" "NOVA" "Het.Zonnetje.in.Huis" "Mr.Deity" "Overspel" "Raveleijn" "Tessa" "Vechtershart" "50.Jaar.van.Duin" "de.Fractie" "Flikken.Rotterdam" "Mindf_ck" )
+"MythBusters" "NOVA" "Het.Zonnetje.in.Huis" "Mr.Deity" "Overspel" "Raveleijn" "Tessa" "Vechtershart" "50.Jaar.van.Duin" "de.Fractie" "Flikken.Rotterdam" "Mindf.ck" )
 # Animation TV shows (hand-drawn content)
 tvanimation=( "Tom.and.Jerry" "Looney.Tunes" "Avatar.the.Legend.of.Korra" "Avatar.the.Last.Airbender" )
 # TV Shows with English subs
