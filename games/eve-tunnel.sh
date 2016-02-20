faeee1cd8ddd61facfad85b14cea7a14a58bbab7
John Gerritse <jgerritse@ebay.com>
2016-02-20 03:14:21 +0100

    Improved version.

--- games/eve-tunnel.sh
+++ games/eve-tunnel.sh
@@ -2,7 +2,7 @@
 # Start an SSH tunnel for EVE Online.
 # Make sure to start your client with the argument "/server:127.0.0.1" in the EVE Launcher.
 
-remotehost="tyhawk@84.104.234.218"
+remotehost="macaw.birdsnest.lan"
 remoteport="22"
 
 # Only start if not running
@@ -17,3 +17,8 @@ if [[ $(ps -ef | grep 26000 | grep "$remotehost" | grep -v grep | wc -l) -ne 1 ]
 else
 	printf "SSH tunnel for EVE Online already running."
 fi
+
+# Start EMDU for localhost cache
+printf "\nStarting EMDU for the proxied EVE Client:\n"
+#emdu_console --enable-deletion --add-eve "/Users/jgerritse/Library/Application\ Support/EVE\ Online/p_drive/Local\ Settings/Application\ Data/CCP/EVE/c_program_files_ccp_eve_127.0.0.1/cache/MachoNet/127.0.0.1/415"
+emdu_console --enable-deletion 
