35cd498f1fb8cd7af3401802b61d50e82c7eb925
John Gerritse <jgerritse@ebay.com>
2016-05-07 16:04:11 +0200

    Now checks 3 dirs for the movie.

--- misc/trailerdownload.sh
+++ misc/trailerdownload.sh
@@ -4,14 +4,39 @@
 listfile="$HOME/Downloads/trailerdownload.lst"
 cp "$listfile" "$listfile.bkp"
 
-# Set the root directory
-rootdir="/mnt/Speelfilm"
 #Default trailer name
 trailername="Official Trailer-trailer.mp4"
 
 deleteline ()
 {
   sed --in-place -e "/$1/d" $listfile
+}
+
+downloadthetrailer()
+{
+  printf "Downloading trailer $moviename ($movieyear) to $trailerdir."
+  # Check if we can download the 720p version
+  if youtube-dl -f 22 --simulate --quiet $trailerlink
+  then
+    printf "."
+    # Download the 720p version
+    if youtube-dl -f 22 --merge-output-format mp4 --quiet $trailerlink --output "$trailerdir/$trailername"
+      printf "."
+    then
+      deleteline $moviename && printf ". OK\n"
+    else
+      printf "."
+      # Download the trailer at its current best quality
+      if youtube-dl --merge-output-format mp4 --quiet $trailerlink --output "$trailerdir/$trailername"
+        printf "."
+      then
+        deleteline $moviename && printf ". OK\n"
+      else
+        printf ". FAILED!"
+      fi
+    fi
+  fi
+  sleep 2m # Give Plex time to scan the new content
 }
 
 # Read the input file
@@ -21,37 +46,17 @@ do
   trailerlink=$(echo $line | awk '{ print $2 }')
   moviename=$(echo $movie | cut --delimiter=\- --fields=1)
   movieyear=$(echo $movie | cut --delimiter=\- --fields=2 | cut -c 2-5 )
-  #trailerdir="$rootdir/$moviename-($movieyear)"
-  trailerdir="$rootdir/$movie"
-  printf "Downloading trailer $moviename ($movieyear) to $trailerdir."
-  if [[ -d "$trailerdir" ]]
+  trailerdir="/mnt/Speelfilm/$movie"
+  if [[ -d "/mnt/Speelfilm/$movie" ]]
   then
-    printf "."
-    # Check if we can download the 720p version
-    if youtube-dl -f 22 --simulate --quiet $trailerlink
-    then
-      printf "."
-      # Download the 720p version
-      if youtube-dl -f 22 --merge-output-format mp4 --quiet $trailerlink --output "$trailerdir/$trailername"
-      printf "."
-      then
-        deleteline $moviename && printf ". OK\n"
-      else
-        printf ". FAILED!\n"
-      fi
-    else
-      printf "."
-      # Download the trailer at its current best quality
-      if youtube-dl --merge-output-format mp4 --quiet $trailerlink --output "$trailerdir/$trailername"
-      printf "."
-      then
-        deleteline $moviename && printf ". OK\n"
-      else
-        printf ". FAILED!\n"
-      fi
-    fi
-    sleep 2m # Give Plex time to settle
+    trailerdir="/mnt/Speelfilm/$movie"
+    downloadthetrailer
+  elif [[ -d "/mnt/SpeelfilmKids/$movie" ]]
+    trailerdir="/mnt/SpeelfilmKids/$movie"
+    downloadthetrailer
+  elif [[ -d "/mnt/Documentaire/$movie" ]]
+    trailerdir="/mnt/Documentaire/$movie"
+    downloadthetrailer
   else
-    printf ". ABORTED!\nMovie not in collection (yet).\n"
-  fi
+    printf "Directory for $moviename ($movieyear) not found. SKIPPING\n"
 done
